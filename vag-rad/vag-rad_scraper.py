a1e9df7ef25bb6fcd9ad66d48d1243a94d58cb10
Bernhard Schiffer <schiffer.be@gmail.com>
2023-05-22 14:53:21 +0200

    change vag-rad api endpoint

--- vag-rad/vag-rad_scraper.py
+++ vag-rad/vag-rad_scraper.py
@@ -2,14 +2,13 @@
 import requests
 import datetime
 import time
-import os
 from datetime import datetime
 from datetime import timedelta
 import pytz
 import logging
 import sys
 import traceback
-
+from fp.fp import FreeProxy
 # %%
 ft = "%Y-%m-%dT%H:%M:%S%z"
 tz = pytz.timezone('UTC')
@@ -23,16 +22,21 @@ def sleep_until(target):
         return True
 
 # %%
-logging.basicConfig(filename='./logs/vag-rad-scraper.log', level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s', datefmt=ft)
-api_url = "https://gbfs.nextbike.net/maps/gbfs/v2/nextbike_dv/de/free_bike_status.json"
+logging.basicConfig(filename='./vag-rad-scraper.log', level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s', datefmt=ft)
+api_url = "https://api.nextbike.net/maps/nextbike-live.json?city=626"
 logging.info('starting scraper')
+
+proxy_servers = {
+   'http': FreeProxy().get(),
+}
+
 while True:
     try:
-        response = requests.get(api_url)
-        timestamp = datetime.fromtimestamp(response.json()['last_updated'], tz)
-        nextupdate = timestamp + timedelta(seconds=response.json()['ttl']+10)
+        response = requests.get(api_url, proxies=proxy_servers)
+        timestamp = datetime.utcnow().replace(tzinfo=pytz.UTC)
+        nextupdate = timestamp + timedelta(seconds=10)
         logging.info('successfully received data')
-        f = open(f"./scraping_data/{timestamp.isoformat()}.json", "w")
+        f = open(f"./scraping_data/rappid_test/{timestamp.isoformat()}.json", "w")
         f.write(response.text)
         f.close()
         logging.info('successfully written data to file')
@@ -40,6 +44,7 @@ while True:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         logging.exception(f'{exc_type} on line {exc_tb.tb_lineno} \n {traceback.format_exc()}')
         nextupdate = datetime.utcnow().replace(tzinfo=pytz.UTC) + timedelta(seconds=10)
+        proxy_servers['http'] = FreeProxy().get()
     finally:
         sleep_until(nextupdate)
 # %%
