ae1d54cca7de6b29853156ed525db9b1e5f0f522
Bernhard Schiffer <schiffer.be@gmail.com>
2023-05-29 21:46:23 +0200

    explore ride distance distribution

--- vag-rad/visualization.py
+++ vag-rad/visualization.py
@@ -23,7 +23,7 @@ conn = psycopg2.connect(
     password=POSTGRES_PASSWORD,
     port=POSTGRES_PORT)
 
-bike_id = 900405
+bike_id = 901857
 sql = f"""select r.* from rides r 
         where r.bike_id = '{bike_id}' 
         order by r.starting_time;"""
@@ -44,10 +44,38 @@ df.drop('finishing_position', axis=1, inplace=True)
 df = df.merge(finishing_pos, on='id')
 conn.close()
 
+#%%
+conn = psycopg2.connect(
+    host=POSTGRES_HOST,
+    database=POSTGRES_DB,
+    user=POSTGRES_USER,
+    password=POSTGRES_PASSWORD,
+    port=POSTGRES_PORT)
+
+sql = f"""select r.* from rides r 
+        where ST_Distance(starting_position, finishing_position) <= 150
+        and ST_Distance(starting_position, finishing_position) >= 100;"""
+finishing_pos_sql = f"""select r.id, r.finishing_position from rides r
+                        where ST_Distance(starting_position, finishing_position) <= 150
+                        and ST_Distance(starting_position, finishing_position) >= 100;"""
+df = gpd.read_postgis(
+    sql, 
+    conn, 
+    geom_col='starting_position', 
+    parse_dates='%Y-%m-%d %H:%M:%S')
+
+finishing_pos = gpd.read_postgis(
+    finishing_pos_sql, 
+    conn, 
+    geom_col='finishing_position')
+df.drop('finishing_position', axis=1, inplace=True)
+df = df.merge(finishing_pos, on='id')
+conn.close()
+
 # %%
 df
 # %%
-map = folium.Map(location=[49.451900, 11.076608], zoom_start=12)
+map = folium.Map(location=[49.451900, 11.076608], zoom_start=12, crs='EPSG3857')
 
 for index, row in df.iterrows():
     starting_position = (row['starting_position'].x, row['starting_position'].y)
@@ -59,4 +87,28 @@ for index, row in df.iterrows():
 
 map
 
+# %%
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+conn = psycopg2.connect(
+    host=POSTGRES_HOST,
+    database=POSTGRES_DB,
+    user=POSTGRES_USER,
+    password=POSTGRES_PASSWORD,
+    port=POSTGRES_PORT)
+
+df = pd.read_sql_query('select ST_Distance(r.starting_position, r.finishing_position) as dist from rides r where ST_Distance(r.starting_position, r.finishing_position) < 10000 order by dist desc;',con=conn)
+
+conn.close()
+# %%
+df
+# %%
+df['Distances'] = pd.qcut(df['dist'], [0, 0.25, 0.5, 0.75, 1])
+print(df.head())
+
+# %%
+plt.figure();
+df['dist'][:].plot.hist(bins=1000, logy=True)
 # %%
