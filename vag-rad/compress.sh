e3302dd0b4e7a591957b2673ea49f30a31ddffd6
Bernhard Schiffer <schiffer.be@gmail.com>
2023-06-01 11:12:21 +0200

    do not overwrite existing archives

--- vag-rad/compress.sh
+++ vag-rad/compress.sh
@@ -1,1 +1,10 @@
-tar -czvf $(date --date="yesterday" +%Y-%m-%d).tar.gz $(date --date="yesterday" +%Y-%m-%d)T*.json --remove-files
+#!/bin/bash
+
+d=$(date --date="yesterday" +%Y-%m-%d)
+
+if [ -f $d.tar.gz ]; then
+    echo "Oops! backup file for the $d was already here."
+    exit
+fi
+
+tar -czvf $d.tar.gz $d*.json --remove-files
