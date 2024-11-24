75f17a0254c7b17950f23f68248bd494d18863d5
BernhardSchiffer <schiffer.be@gmail.com>
2024-11-24 17:45:29 +0100

    plot subgraphs in one map

--- vag-rad/bike_network_analysis.py
+++ vag-rad/bike_network_analysis.py
@@ -1,6 +1,9 @@
 #%% imports
+import matplotlib.colors
 import osmnx as ox
 import networkx as nx
+import folium
+import matplotlib
 
 # %% helper function
 # calculate length of edges of a graph
@@ -11,6 +14,16 @@ def get_path_length(graph):
     else:
         return 0
 
+def plot_graph(graph, map=folium.Map(location=[49.451900, 11.076608], zoom_start=12, crs='EPSG3857'), color='blue'):
+    if(len(graph.edges) > 0):
+        df = ox.graph_to_gdfs(graph, nodes=False)
+        for t in df['geometry'].values:
+            coordinates = []
+            for c in t.coords[:]:
+                coordinates.append((c[1], c[0]))
+            folium.PolyLine(coordinates, color=color).add_to(map)
+    return map
+
 # %%
 place_name = 'Nürnberg'
 graph = ox.graph_from_place(place_name)
@@ -20,8 +33,8 @@ fig, ax = ox.plot_graph(graph)
 # %% fetch graph of bicycle infrastructure
 place_name = 'Nürnberg'
 network_type = 'bike'
-custome_filter = '["highway"="path"]["bicycle"=designated]'
-graph = ox.graph_from_place(query=place_name, retain_all=True, custom_filter=custome_filter)
+custom_filter = '["highway"="path"]["bicycle"=designated]'
+graph = ox.graph_from_place(query=place_name, retain_all=True, custom_filter=custom_filter)
 
 # %% some statistics of the whole graph
 edges = ox.graph_to_gdfs(graph, nodes=False)
@@ -54,9 +67,18 @@ sorted_components_by_length = sorted(list_of_components, key=lambda d: d['length
 
 print('top 10 of connected components by length')
 for sub in sorted_components_by_length[:10]:
-    map = ox.graph_to_gdfs(sub['graph'], nodes=False).explore()
-    display(map)
-    print(f'number of edges: {len(sub["graph"].edges)}')
+    edges = ox.graph_to_gdfs(sub['graph'], nodes=False)
+    display(edges.explore())
+    print(f'number of edges: {len(edges)}')
     print(f'length of component: {sub["length"]} meters')
+
+# %% plot components on one map
+cmap = matplotlib.cm.get_cmap('tab10')
+map = folium.Map(location=[49.451900, 11.076608], zoom_start=11, crs='EPSG3857')
+for idx, c in enumerate(sorted_components_by_length):
+    color = matplotlib.colors.to_hex(cmap(idx%10))
+    plot_graph(c['graph'], map=map, color=color)
+
+map
 
 # %%
