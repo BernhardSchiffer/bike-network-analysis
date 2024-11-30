45592c1991e75a14c72197d1215257ccced1f1f5
BernhardSchiffer <schiffer.be@gmail.com>
2024-11-30 16:33:21 +0100

    add analysis of osm tags

--- vag-rad/bike_network_analysis.py
+++ vag-rad/bike_network_analysis.py
@@ -1,12 +1,19 @@
-#%% imports
+# %% 
+# imports
 import matplotlib.colors
+import osmium.filter
+import osmium.osm
 import osmnx as ox
 import networkx as nx
 import folium
 import matplotlib
 import matplotlib.pyplot as plt
+import osmium
+from collections import Counter
+import shapely
 
-# %% helper function
+# %%
+# helper functions
 # calculate length of edges of a graph
 def get_path_length(graph):
     if len(graph.edges) > 0:
@@ -26,7 +33,63 @@ def plot_graph(graph, map=folium.Map(location=[49.451900, 11.076608], zoom_start
             folium.PolyLine(coordinates, color=color).add_to(map)
     return map
 
-# %% fetch graph of bicycle infrastructure
+# osmium filter that checks if node or one of way nodes is in polygon
+class PolygonFilter:
+    def __init__(self, polygon):
+        self.polygon = polygon
+
+    def node(self, n):
+        is_not_in_place = True
+        if n.location.valid() and self.polygon.contains(shapely.Point(n.lon, n.lat)):
+            is_not_in_place = False
+        return is_not_in_place
+
+    def way(self, w):
+        is_not_in_place = True
+        for n in w.nodes:
+            if n.location.valid() and self.polygon.contains(shapely.Point(n.lon, n.lat)):
+                is_not_in_place = False
+                break
+        return is_not_in_place
+
+# %% 
+# analysis of osm features in Nürnberg
+print("Total number of objects in Mittelfranken:", sum(1 for o in osmium.FileProcessor('mittelfranken-latest.osm.pbf')))
+
+print("Of which are ways with tags:", sum(1 for o in osmium.FileProcessor('mittelfranken-latest.osm.pbf').with_filter(osmium.filter.EmptyTagFilter()).with_filter(osmium.filter.EntityFilter(osmium.osm.WAY))))
+
+place = ox.geocode_to_gdf('Nürnberg')
+print("Of which are ways within Nürnberg:",
+      sum(1 for o in osmium.FileProcessor('mittelfranken-latest.osm.pbf').with_locations().with_filter(osmium.filter.EmptyTagFilter()).with_filter(osmium.filter.EntityFilter(osmium.osm.WAY)).with_filter(PolygonFilter(place.geometry[0]))))
+
+# %%
+# get all osm tags of ways in Nürnberg
+place = ox.geocode_to_gdf('Nürnberg')
+stats = Counter()
+
+for w in osmium.FileProcessor('mittelfranken-latest.osm.pbf').with_locations().with_filter(osmium.filter.EmptyTagFilter()).with_filter(osmium.filter.EntityFilter(osmium.osm.WAY)).with_filter(PolygonFilter(place.geometry[0])):
+    for k, v in w.tags:
+        stats.update([(k, v)])
+
+# %%
+# get all bicycle related tags in Nürnberg
+bicycle_tags = []
+for (key, value) in stats.keys():
+    if('cycle' in key or 'cycle' in value):
+        bicycle_tags.append((key, value))
+
+print(f'there are {len(bicycle_tags)} tags bicycle related tags')
+
+tmp = {}
+for k, v in stats.items():
+    if(k in bicycle_tags):
+        tmp[k] = v
+
+bicycle_stats = Counter(tmp)
+bicycle_stats.most_common(len(bicycle_stats))
+
+# %% 
+# fetch graph of bicycle infrastructure
 place_name = 'Nürnberg'
 network_type = 'bike'
 bike_lane_filter = [
@@ -44,33 +107,24 @@ bike_path_filter = [
 bike_road_filter = [
     '["bicycle_road"="yes"]'
 ]
-custom_filter = bike_path_filter + bike_lane_filter + bike_road_filter
+custom_filter = bike_path_filter
 graph = ox.graph_from_place(query=place_name, retain_all=True, custom_filter=custom_filter)
 
-# %% some statistics of the whole graph
+# %% 
+# some statistics of the graph
 edges = ox.graph_to_gdfs(graph, nodes=False)
 overall_length = sum(edges["length"])
 display(edges.explore())
 print(f'number of edges: {len(edges)}')
 print(f'length of network: {overall_length} meters')
-# %% explore connected components in graph
+# %% 
+# explore connected components in graph
 undirected_graph = graph.to_undirected()
 
 print(f'number of connected components: {nx.number_connected_components(undirected_graph)}')
 
-# %% top connected components by number of edges
-S = [undirected_graph.subgraph(c).copy() for c in sorted(nx.connected_components(undirected_graph), key=len, reverse=True)]
-
-print('top 10 of connected components by number of edges')
-for sub in S[:10]:
-    edges = ox.graph_to_gdfs(sub, nodes=False, edges=True)
-
-    map = edges.explore()
-    display(map)
-    print(f'number of edges: {len(edges)}')
-    print(f'length of component: {sum(edges["length"])} meters')
-
-# %% top connected components by length
+# %% 
+# find all connected components in graph
 list_of_components = []
 
 for c in nx.connected_components(undirected_graph):
@@ -79,7 +133,8 @@ for c in nx.connected_components(undirected_graph):
 
 sorted_components_by_length = sorted(list_of_components, key=lambda d: d['length'], reverse=True)
 
-# %% plot components on one map
+# %% 
+# plot all connected components on one map
 cmap = matplotlib.cm.get_cmap('tab10')
 map = folium.Map(location=[49.451900, 11.076608], zoom_start=11, crs='EPSG3857')
 
@@ -89,7 +144,8 @@ for idx, c in enumerate(sorted_components_by_length):
 
 map
 
-# %% top 10 of connected components by length
+# %% 
+# top 10 of connected components by length
 print('top 10 of connected components by length')
 for sub in sorted_components_by_length[:10]:
     edges = ox.graph_to_gdfs(sub['graph'], nodes=False)
@@ -99,6 +155,7 @@ for sub in sorted_components_by_length[:10]:
     print(f'{(sum(edges["length"])/overall_length)*100}% of whole network')
 
 # %% 
+# statistics of connected components
 lengths = []
 
 for c in list_of_components:
