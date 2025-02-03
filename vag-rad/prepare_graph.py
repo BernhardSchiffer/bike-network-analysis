a7fe8c2de2f0a9d1498f28243f95433bb93e0f64
BernhardSchiffer <schiffer.be@gmail.com>
2025-02-03 10:42:27 +0100

    apply weights to graph edges according to osm features, turning angles and elevation

--- vag-rad/prepare_graph.py
+++ vag-rad/prepare_graph.py
@@ -7,12 +7,16 @@ import os
 import folium
 import time
 import osmium
+import math
 from utils.polygon_filter import PolygonFilter
 from utils.utils import *
 import pickle
 import leafmap.foliumap as leafmap
 from pyproj import Transformer
 from shapely.geometry import LineString
+import matplotlib.pyplot as plt
+import matplotlib
+import igraph as ig
 
 osm_to_geotiff = Transformer.from_crs("EPSG:4326", "EPSG:25832")
 geotiff_to_osm = Transformer.from_crs("EPSG:25832", "EPSG:4326")
@@ -28,11 +32,33 @@ def get_elevation(lon, lat):
     return dat.xy(*idx), z[idx]
 
 # %%
-# use specific overpass settings
-ox.settings.overpass_settings = '[out:json][timeout:{timeout}][date:"2024-11-30T00:00:00Z"]{maxsize}'
-# %% 
-# use default overpass settings
-ox.settings.overpass_settings = '[out:json][timeout:{timeout}]{maxsize}'
+# load calculated routes from file
+edge_lookup_filename = 'osm_edges_with_attributes.pickle'
+
+if os.path.isfile(edge_lookup_filename):
+    with open(edge_lookup_filename, 'rb') as f:
+        edges_osmdata_lookup = pickle.load(f)
+else:
+    # create lookup table for all edges in nuernberg with all their osm features
+    place = ox.geocode_to_gdf('Nürnberg')
+
+    edges_in_nbg = []
+
+    for w in osmium.FileProcessor('mittelfranken-latest.osm.pbf').with_locations().with_filter(osmium.filter.EmptyTagFilter()).with_filter(osmium.filter.EntityFilter(osmium.osm.WAY)).with_filter(PolygonFilter(place.geometry[0])):
+        obj = {}
+        obj['osmid'] = w.id
+        tags = {}
+        for k, v in w.tags:
+            tags[k] = v
+        obj['tags'] = tags
+        edges_in_nbg.append(obj)
+
+    edges_osmdata_lookup = pd.DataFrame(edges_in_nbg).set_index('osmid')
+
+    # write calculated routes on file
+    file = open(edge_lookup_filename, 'wb')
+    pickle.dump(edges_osmdata_lookup, file)
+    file.close()
 
 # %%
 # set node attributes
@@ -77,7 +103,7 @@ def set_edge_slope(graph: nx.DiGraph) -> nx.DiGraph:
         hight_diff = dest_node['elevation'] - start_node['elevation']
         slope_percentage = (hight_diff / e_data['length']) * 100
 
-        slope_percentages[u, v] = {'slope_percentage': slope_percentage}
+        slope_percentages[u, v] = {'slope_percentage': float(slope_percentage)}
         
     nx.set_edge_attributes(graph, slope_percentages)
 
@@ -135,10 +161,12 @@ def split_nodes(graph: nx.DiGraph) -> nx.DiGraph:
             for out_node, out_edge in zip(out_nodes, out_edges):
                 out_edge_data = out_edge[2]
                 # TODO calculate turn angle and set attribute if street is the same or changes
-                e1 = edge_lookup.loc[in_edge_data['old_edge_key'][0], in_edge_data['old_edge_key'][1], 0]['geometry']
-                e2 = edge_lookup.loc[out_edge_data['old_edge_key'][0], out_edge_data['old_edge_key'][1], 0]['geometry']
-                turning_angle = get_angle_between_edges(e1, e2)
-                graph.add_edge((in_edge_start, in_edge_dest), out_node, length=0.0, turning_angle=turning_angle)
+                e1 = edge_lookup.loc[in_edge_data['old_edge_key'][0], in_edge_data['old_edge_key'][1], 0]
+                e2 = edge_lookup.loc[out_edge_data['old_edge_key'][0], out_edge_data['old_edge_key'][1], 0]
+                turning_angle = get_angle_between_edges(e1['geometry'], e2['geometry'])
+                weight = get_turn_penalty(turning_angle) - 1.0
+                length = float(e1['length'] * weight)
+                graph.add_edge((in_edge_start, in_edge_dest), out_node, length=length, turning_angle=turning_angle)
             # add edge from previous node to new in node
             graph.add_edges_from([((in_edge_start, in_edge_dest)[0], (in_edge_start, in_edge_dest), in_edge_data)])
         # remove old node
@@ -159,75 +187,113 @@ def debug_plot(graph: nx.DiGraph):
 
     return map
 
-# %% 
-# load calculated routes from file
-edge_lookup_filename = 'osm_edges_with_attributes.pickle'
+def is_tag_available(attribute: str, value: str, tags: dict[str, str]) -> bool:
+    if attribute not in tags.keys():
+        return False
+    else:
+        return tags[attribute] == value
+# %%
+def get_slope_penalty(slope: float) -> float:
+    if slope < 2:
+        return 1.0
+    elif slope >= 2 and slope < 4:
+        return 1.37
+    elif slope >= 4 and slope < 6:
+        return 2.2
+    elif slope >= 6:
+        return 4.24
 
-if os.path.isfile(edge_lookup_filename):
-    with open(edge_lookup_filename, 'rb') as f:
-        edges_lookup = pickle.load(f)
-else:
-    # create lookup table for all edges in nuernberg with all their osm features
-    place = ox.geocode_to_gdf('Nürnberg')
+class TurnDirection:
+    LEFT = -1
+    STRAIGHT = 0
+    RIGHT = 1
+    U_TURN = 2
 
-    edges_in_nbg = []
+def get_turn_direction(turn_angle: float) -> TurnDirection:
+    if turn_angle < -135:
+        return TurnDirection.U_TURN
+    elif turn_angle >= -135 and turn_angle < -45:
+        return TurnDirection.LEFT
+    elif turn_angle >= -45 and turn_angle < 45:
+        return TurnDirection.STRAIGHT
+    elif turn_angle >= 45 and turn_angle < 135:
+        return TurnDirection.RIGHT
+    elif turn_angle >= 135:
+        return TurnDirection.U_TURN
 
-    for w in osmium.FileProcessor('mittelfranken-latest.osm.pbf').with_locations().with_filter(osmium.filter.EmptyTagFilter()).with_filter(osmium.filter.EntityFilter(osmium.osm.WAY)).with_filter(PolygonFilter(place.geometry[0])):
-        obj = {}
-        obj['osmid'] = w.id
-        tags = {}
-        for k, v in w.tags:
-            tags[k] = v
-        obj['tags'] = tags
-        edges_in_nbg.append(obj)
+def get_turn_penalty(turn_angle: float) -> float:
+    turn_direction = get_turn_direction(turn_angle)
+    if turn_direction == TurnDirection.STRAIGHT:
+        return 1.0001
+    elif turn_direction == TurnDirection.LEFT:
+        return 1.04
+    elif turn_direction == TurnDirection.RIGHT:
+        return 1.04
+    elif turn_direction == TurnDirection.U_TURN:
+        return 1.0001
 
-    edges_lookup = pd.DataFrame(edges_in_nbg).set_index('osmid')
+def has_bike_lane(tags: dict[str, str]) -> bool:
+    bike_lane_filter: list[tuple[str, str]] = [
+        ("cycleway", "lane"),
+        ("cycleway:right", "lane"),
+        ("cycleway:left", "lane"),
+        ("cycleway:both", "lane"),
+        ("cycleway", "opposite")
+    ]
+    return any(is_tag_available(k, v, tags) for k, v in bike_lane_filter)
 
-    # write calculated routes on file
-    file = open(edge_lookup_filename, 'wb')
-    pickle.dump(edges_lookup, file)
-    file.close()
-    
+def has_bike_path(tags: dict[str, str]) -> bool:
+    bike_path_filter: list[tuple[str, str]] = [
+        ("bicycle", "designated"),
+        ("highway", "cycleway"),
+        ("cycleway", "track"),
+        ("cycleway:right", "track"),
+        ("cycleway:left", "track"),
+        ("cycleway:both", "track")
+    ]
+    return any(is_tag_available(k, v, tags) for k, v in bike_path_filter)
+
+def is_bike_road(tags: dict[str, str]) -> bool:
+    bike_road_filter: list[tuple[str, str]] = [
+        ('bicycle_road', 'yes')
+    ]
+    return any(is_tag_available(k, v, tags) for k, v in bike_road_filter)
+
+def is_primary_road(tags: dict[str, str]) -> bool:
+    primary_road_filter: list[tuple[str, str]] = [
+        ('highway', 'primary')
+    ]
+    return any(is_tag_available(k, v, tags) for k, v in primary_road_filter)
+
+def is_secondary_road(tags: dict[str, str]) -> bool:
+    secondary_road_filter: list[tuple[str, str]] = [
+        ('highway', 'secondary')
+    ]
+    return any(is_tag_available(k, v, tags) for k, v in secondary_road_filter)
+
+def is_tertiary_road(tags: dict[str, str]) -> bool:
+    tertiary_road_filter: list[tuple[str, str]] = [
+        ('highway', 'tertiary')
+    ]
+    return any(is_tag_available(k, v, tags) for k, v in tertiary_road_filter)
+
+def is_residential_road(tags: dict[str, str]) -> bool:
+    residential_road_filter: list[tuple[str, str]] = [
+        ('highway', 'residential')
+    ]
+    return any(is_tag_available(k, v, tags) for k, v in residential_road_filter)
+
 # %%
 # define benefits and penalties for edges according to their osm features
-bike_lane_filter: list[tuple[str, str]] = [
-    ("cycleway", "lane"),
-    ("cycleway:right", "lane"),
-    ("cycleway:left", "lane"),
-    ("cycleway:both", "lane"),
-    ("cycleway", "opposite")
-]
-bike_path_filter: list[tuple[str, str]] = [
-    ("bicycle", "designated"),
-    ("highway", "cycleway"),
-    ("cycleway", "track"),
-    ("cycleway:right", "track"),
-    ("cycleway:left", "track"),
-    ("cycleway:both", "track")
-]
-bike_road_filter: list[tuple[str, str]] = [
-    ('bicycle_road', 'yes')
-]
-primary_road_filter: list[tuple[str, str]] = [
-    ('highway', 'primary')
-]
-secondary_road_filter: list[tuple[str, str]] = [
-    ('highway', 'secondary')
-]
-tertiary_road_filter: list[tuple[str, str]] = [
-    ('highway', 'tertiary')
-]
-residential_road_filter: list[tuple[str, str]] = [
-    ('highway', 'residential')
-]
+type filter = tuple[list[function], float]
 
-bike_lanes_separate = (bike_path_filter, 0.84)
-bike_lanes_on_road = (bike_lane_filter, 0.84)
-bike_boulevard = (bike_road_filter, 0.90)
-primary_road = (primary_road_filter, 8.15)
-secondary_road = (secondary_road_filter, 2.40)
-tertiary_road = (tertiary_road_filter, 1.37)
-residential_road = (residential_road_filter, 1.10)
+bike_lanes_separate: filter = ([has_bike_path], 0.84)
+bike_lanes_on_road: filter = ([has_bike_lane], 0.84)
+bike_boulevard: filter = ([is_bike_road], 0.90)
+primary_road: filter = ([is_primary_road], 8.15)
+secondary_road: filter = ([is_secondary_road], 2.40)
+tertiary_road: filter = ([is_tertiary_road], 1.37)
+residential_road: filter = ([is_residential_road], 1.10)
 
 benefit_lookup = [
     bike_lanes_separate,
@@ -239,59 +305,43 @@ benefit_lookup = [
     residential_road
 ]
 
-def is_tag_available(attribute: str, value: str, tags: dict[str, str]) -> bool:
-    if attribute not in tags.keys():
-        return False
-    else:
-        return tags[attribute] == value
-    
-def any_attributes_present(filter_tags: tuple[str, str], edge_tags: dict[str, str]):
-    return any(is_tag_available(k, v, edge_tags) for k, v in filter_tags)
 
-
-def get_weight(osmid: int) -> float:
+def get_weight(u, v, data) -> float:
     try:
-        tags = edges_lookup.loc[osmid, 'tags']
+        osmid = data['osmid']
+        tags = edges_osmdata_lookup.loc[osmid, 'tags']
     except:
         raise ValueError(f'could not find edge with osmid {osmid}')
     
-    b = None
-    for filter_tags, benefit in benefit_lookup:
-        if any_attributes_present(filter_tags, tags) and (b is None or benefit < b):
-            b = benefit
+    penalties: float = []
 
-    if b is not None:
-        return b
-    else:
-        return 1.0
-
-# %%
-# calculate edge weights according to their osm features
-print(f'starting to calculate edges weights')
-start = time.time()
-weights: dict[tuple[int, int], dict[str, float]] = {}
-problematic_osmids = []
-for u, v, k in graph.edges:
-    data = graph.edges[u,v,k]
     try:
-        w = get_weight(data['osmid'])
-        weights[u,v,k] = {'weight': data['length'] * w}
+        penalties.append(get_slope_penalty(data['slope_percentage']))
     except:
-        problematic_osmids.append(data['osmid'])
+        pass
 
-end = time.time()
-print(f'successfully calculated weight of {len(weights)} edges in {end - start} seconds')
+    try:
+        penalties.append(get_turn_penalty(data['turning_angle']))
+    except:
+        pass
 
-if len(problematic_osmids) > 0:
-    print(f'found problems with {len(problematic_osmids)} edges')
-    get_list_of_edges(problematic_osmids, edges_lookup).explore()
+    for filter_functions, benefit in benefit_lookup:
+        for f in filter_functions:
+            if f(tags):
+                penalties.append(benefit)
+
+    if len(penalties) == 0:
+        return 1.0
+    else:
+        return math.prod(penalties)
 
 # %%
-# add weight attribute to graph
-nx.set_edge_attributes(graph, weights)
-
+# use specific overpass settings
+ox.settings.overpass_settings = '[out:json][timeout:{timeout}][date:"2024-11-30T00:00:00Z"]{maxsize}'
+# %% 
+# use default overpass settings
+ox.settings.overpass_settings = '[out:json][timeout:{timeout}]{maxsize}'
 # %%
-
 # fetch graph of all streets available by bike
 place_name = 'Nürnberg'
 
@@ -315,6 +365,35 @@ graph = set_node_attributes(graph)
 graph = set_node_elevation(graph)
 graph = set_edge_slope(graph)
 graph = split_nodes(graph)
+
+#%%
+# calculate edge weights according to their osm features
+print(f'starting to calculate edges weights')
+start = time.time()
+weights: dict[tuple[int, int], dict[str, float]] = {}
+problematic_osmids = []
+for u, v, data in graph.edges(data=True):
+    if 'osmid' not in data.keys():
+        weights[u,v] = {'weight': data['length']}
+        continue
+
+    try:
+        weight = get_weight(u, v, data)
+        weights[u,v] = {'weight': float(data['length'] * weight)}
+    except:
+        problematic_osmids.append(data['osmid'])
+
+end = time.time()
+print(f'successfully calculated weight of {len(weights)} edges in {end - start} seconds')
+
+if len(problematic_osmids) > 0:
+    print(f'found problems with {len(problematic_osmids)} edges')
+    get_list_of_edges(problematic_osmids, edges_osmdata_lookup).explore()
+
+# %%
+# add weight attribute to graph
+nx.set_edge_attributes(graph, weights)
+
 #%% 
 print('edges:', len(graph.edges))
 #for edge in graph.edges(data=True):
@@ -326,4 +405,27 @@ print('nodes:', len(graph.nodes))
 # %%
 debug_plot(graph).save('debug.html')
 
+# %%
+wg: ig.Graph = ig.Graph.from_networkx(graph)
+
+# %%
+ebc = wg.edge_betweenness(directed = False, cutoff = 4500, weights = "weight")
+
+# %%
+nodes = ox.graph_to_gdfs(nx.MultiDiGraph(graph), nodes=True, edges=False, node_geometry=True, fill_edge_geometry=False)
+cmap = plt.get_cmap('turbo')
+map = leafmap.Map(location=[49.451900, 11.076608], zoom_start=12, crs='EPSG3857')
+
+max_value = max(ebc)
+
+for edge, count in zip(graph.edges(), ebc):
+    positions = []
+    for node_id in edge:
+        node = graph.nodes[node_id]
+        positions.append((node['y'], node['x']))
+    color = matplotlib.colors.to_hex(cmap(count/max_value))
+    folium.PolyLine(positions, color=color, tooltip=count).add_to(map)
+
+map.add_colormap(position=(55,3), width=4.0, height=0.3, vmin=0, vmax=max_value, cmap='turbo')
+map.save('ebc_complex_weighted.html')
 # %%
